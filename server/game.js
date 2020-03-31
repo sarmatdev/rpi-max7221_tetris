4825dd76aa11a67c56e58a3b613cef8d4cf48e79
Sarmat Khachirov <skhachirov12@gmail.com>
2020-03-31 22:58:30 +0300

    Implement figure rotating

--- server/game.js
+++ server/game.js
@@ -16,6 +16,9 @@ class Game {
     this.activePiece = {
       x: 0,
       y: 0,
+      get blocks() {
+        return this.rotations[this.rotationIndex];
+      },
       blocks: [
         [0, 1, 0],
         [1, 1, 1],
@@ -49,6 +52,38 @@ class Game {
     }
   }
 
+  rotatePiece() {
+    this.rotateBloks();
+    if (this.hasCollision()) {
+      this.rotateBloks(false);
+    }
+  }
+
+  rotateBloks(clockwise = true) {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    const x = Math.floor(length / 2);
+    const y = length - 1;
+
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y - i; j++) {
+        const temp = blocks[i][j];
+
+        if (clockwise) {
+          blocks[i][j] = blocks[y - j][i];
+          blocks[y - j][i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[j][y - i];
+          blocks[j][y - i] = temp;
+        } else {
+          blocks[i][j] = blocks[j][y - i];
+          blocks[j][y - i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[y - j][i];
+          blocks[y - j][i] = temp;
+        }
+      }
+    }
+  }
+
   hasCollision() {
     const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
@@ -56,8 +91,9 @@ class Game {
       for (let x = 0; x < blocks[y].length; x++) {
         if (
           blocks[y][x] &&
-          ((this.playfield[pieceY + y] === undefined ||
-            this.playfield[pieceY + y][pieceX + x] === undefined) || this.playfield[pieceY + y][pieceX + x])
+          (this.playfield[pieceY + y] === undefined ||
+            this.playfield[pieceY + y][pieceX + x] === undefined ||
+            this.playfield[pieceY + y][pieceX + x])
         ) {
           return true;
         }
